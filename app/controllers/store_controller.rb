05bdc6f37fa427e0bb2a697ebde8111573545020
DionisRuby <gdionis21@gmail.com>
2014-03-28 16:35:41 +0400

    Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  def session_count
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	end
+  	session[:counter] += 1
+  end
+
   def index
+  	@count = session_count
   	@products = Product.order(:title)
+ 	@show_count = "You've bean here #{@count} #{'time'.pluralize}" if @count >= 5
   end
 end
